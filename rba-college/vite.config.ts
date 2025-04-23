3632b715f04b3acacaf84686a356e954bebaadea
Mushahid Khan <mushahid.khan@acxiomconsulting.com>
2025-04-23 12:31:36 +0530

    code added for add nojeklly file in dist published folder

--- rba-college/vite.config.ts
+++ rba-college/vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RBA_College_Proj/',})
+  base: '/RBA_College_Proj/', // Ensure this matches your repository name
+})
